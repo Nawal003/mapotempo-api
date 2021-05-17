f2cfeeb6cb6bc19b91954e9fe68f4425765ffc51
Nawal003 <nawal.zouihig@gmail.com>
2021-05-17 14:02:50 +0200

    api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d792449896aa72eeec308164201f944'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '246489d9-5be3-4916-a7ff-05c4b63e010b'
+};
